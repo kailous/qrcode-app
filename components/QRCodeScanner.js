6daf191f5b97ded6f0f43d22a9ddb3742ecf393b
kailous <kailous@live.cn>
2024-02-04 01:22:40 +0800

    超级里程碑，功能细节完成。

--- components/QRCodeScanner.js
+++ components/QRCodeScanner.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import jsQR from 'jsqr';
 
 const QRCodeScanner = ({ onDecode }) => {
   const [dragging, setDragging] = useState(false);
+  const fileInputRef = useRef(null); // 使用 useRef 钩子来引用文件输入元素
 
   const handleDragOver = (event) => {
     event.preventDefault();
@@ -18,6 +19,17 @@ const QRCodeScanner = ({ onDecode }) => {
     event.preventDefault();
     setDragging(false);
     const files = event.dataTransfer.files;
+    if (files.length) {
+      decodeQRCodeFromFile(files[0]);
+    }
+  };
+
+  const handleClick = () => {
+    fileInputRef.current.click(); // 在点击区域时触发文件输入元素的点击事件
+  };
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
     if (files.length) {
       decodeQRCodeFromFile(files[0]);
     }
@@ -48,12 +60,20 @@ const QRCodeScanner = ({ onDecode }) => {
 
   return (
     <div
+      onClick={handleClick}
       onDragOver={handleDragOver}
       onDragLeave={handleDragLeave}
       onDrop={handleDrop}
       style={{ border: dragging ? '2px dashed #000' : '2px dashed #ccc', padding: '20px', textAlign: 'center', cursor: 'pointer' }}
     >
       {dragging ? '放到这里' : '点击或拖拽图片到此处'}
+      <input
+        type="file"
+        accept="image/*"
+        style={{ display: 'none' }} // 隐藏文件输入元素
+        onChange={handleFileChange}
+        ref={fileInputRef} // 将引用附加到文件输入元素
+      />
     </div>
   );
 };
